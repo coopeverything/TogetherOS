c013026dfdc984a6bad80ebd4fedb4361f157a10
TheEpicuros <132305976+TheEpicuros@users.noreply.github.com>
2025-09-20 13:37:43 -0700

    chore: add scripts/lint.sh [path-a]

---
+++ scripts/lint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+shopt -s nullglob globstar
+
+yamllint_paths=(
+  .github/workflows/**/*.yml
+  .github/workflows/**/*.yaml
+)
+
+yaml_files=()
+for path in "${yamllint_paths[@]}"; do
+  for file in $path; do
+    yaml_files+=("$file")
+  done
+done
+
+if ((${#yaml_files[@]} > 0)); then
+  yamllint "${yaml_files[@]}"
+fi
+
+actionlint -ignore 'github\.event\.issue\.body'
+
+echo "LINT=OK"
